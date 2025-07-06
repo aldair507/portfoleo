16b98064f5c065836e42196bdbc9dc1e49551ab5
Aldair <aldairtorres507@gmail.com>
2025-07-05 19:21:45 -0500

    cambios

--- portfoleo/src/componentes/PorfoleoSection.jsx
+++ portfoleo/src/componentes/PorfoleoSection.jsx
@@ -601,7 +601,10 @@ const Portfolio = () => {
                       d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
                     />
                   </svg>
-                  <a href="/portfoleo/public/Hoja_de_vida_Aldair.pdf" download>
+                  <a
+                    href="https://drive.google.com/file/d/1YhYbMeJIOm1ttVjxBoa58rK5QNyy3OQf/view?usp=drive_link"
+                    target="blank"
+                  >
                     <span>Descargar CV</span>
                   </a>
                 </button>
